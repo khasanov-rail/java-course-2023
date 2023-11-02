9153ae8612edf9e5050518526eb02efa97e204aa
khasanov-rail <rail.khasanov@gmail.com>
2023-11-02 07:20:22 +0600

    hw 3 исправил

--- src/main/java/edu/hw3/Task1.java
+++ src/main/java/edu/hw3/Task1.java
@@ -8,14 +8,18 @@ public class Task1 {
     public static String atbash(String input) {
         StringBuilder result = new StringBuilder();
         for (char c : input.toCharArray()) {
-            char atbashChar = c;
-            if (c >= 'a' && c <= 'z') {
-                atbashChar = (char) ('a' + 'z' - c);
-            } else if (c >= 'A' && c <= 'Z') {
-                atbashChar = (char) ('A' + 'Z' - c);
-            }
+            char atbashChar = convertChar(c);
             result.append(atbashChar);
         }
         return result.toString();
+    }
+
+    private static char convertChar(char c) {
+        if (c >= 'a' && c <= 'z') {
+            return (char) ('a' + 'z' - c);
+        } else if (c >= 'A' && c <= 'Z') {
+            return (char) ('A' + 'Z' - c);
+        }
+        return c;
     }
 }
--- src/main/java/edu/hw3/Task4.java
+++ src/main/java/edu/hw3/Task4.java
@@ -5,6 +5,14 @@ public class Task4 {
     private Task4() {
     }
 
+    private static final int[] VALUES = {
+        1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1
+    };
+
+    private static final String[] ROMAN_LITERALS = {
+        "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"
+    };
+
     @SuppressWarnings("MagicNumber")
     public static String convertToRoman(int num) {
         if (num < 1 || num > 3999) {
@@ -12,14 +20,11 @@ public class Task4 {
         }
 
         StringBuilder roman = new StringBuilder();
-        int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
-        String[] romanLiterals = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
-
         int number = num;
-        for (int i = 0; i < values.length; i++) {
-            while (number >= values[i]) {
-                number -= values[i];
-                roman.append(romanLiterals[i]);
+        for (int i = 0; i < VALUES.length; i++) {
+            while (number >= VALUES[i]) {
+                number -= VALUES[i];
+                roman.append(ROMAN_LITERALS[i]);
             }
         }
 
